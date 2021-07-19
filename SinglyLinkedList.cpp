59f06165f549e7f4dabc69fe6491097003051767
Mohammed sahil ansari <87164152+MdSahilAnsari@users.noreply.github.com>
2021-07-19 07:33:44 -0700

    Add files via upload

---
+++ SinglyLinkedList.cpp
@@ -0,0 +1,70 @@
+
+#include <iostream>
+using namespace std;
+
+class node
+{
+    public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+void insertAtHead(node *&head, int val){
+    node* n=new node(val);
+    n->next=head;
+    head=n;
+}
+void insertAtTail(node *&head, int val)
+{
+
+    node *n = new node(val);
+    if(head==NULL){
+        head=n;
+        return;
+    }
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+void Display(node* head){
+    node* temp=head;
+    while(temp!=NULL){
+        cout<<temp->data<<"->";
+        temp=temp->next;
+
+    }
+    cout<<"NULL"<<endl;
+}
+bool Search(node *&head, int key){
+    node* temp=head;
+    while(temp!=NULL){
+    if(temp->data==key){
+        return true;
+    }
+    temp=temp->next;
+    }
+    return false;
+
+
+}
+int main()
+{
+    node* head=NULL;
+    insertAtTail(head,1);
+     insertAtTail(head,2);
+      insertAtTail(head,3);
+insertAtHead(head,5);
+       Display(head);
+      cout<< Search(head,10);
+    return 0;
+}
